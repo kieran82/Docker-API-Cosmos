0007098cb59dfc78bd6a2475f49025487aaf1993
Kieran <kieran.moroney1982@gmail.com>
2020-02-28 18:35:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-/*﻿
+/*log4js.configure({
+    appenders: {
+        VERIAPI: {
+            type: "file",
+            filename: logpath,
+            maxLogSize: 4194304,
+            backups: 10,
+            keepFileExt: true,
+            compress: true,
+            daysToKeep: 20
+        }
+    },
+    categories: {
+        default: {
+            appenders: ["VERIAPI"],
+            level: "debug"
+        }
+    }
+});
 
-const app = express();
-const bodyParser = require('body-parser');
-const log4js = require('log4js');
+const logger = log4js.getLogger("VERIAPI");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/couchdb', require('./routes/couchdb'));
@@ -11,11 +28,24 @@ app.use('/api/get', require('./routes/getting'));
 app.use('/api/check', require('./routes/checking'));
 let logpath = process.env.LOGFILEPATH;
 
-
+const TaskDao = require("../models/taskDao");
 
 */
+const fs = require("fs");
+const log4js = require("log4js");
+const util = require("util");
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+let contents = fs.readFileSync("site_config.json");
+console.log(contents);
+//let current_path_location = process.env.WORKPATH;
+//let logpath = process.env.LOGFILEPATH;
+
+
+
+
+
 var server = app.listen(8080, function () {
 
     var host = server.address().address
