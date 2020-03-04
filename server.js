34f25f8713637f200d3599162283eb2f9c524458
Kieran Moroney <kieran.moroney1982@gmail.com>
2020-03-04 15:21:40 +0000

    1.0.2

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api/couchdb', require('./routes/couchdb'));
+app.use('/api/blockchain', require('./routes/blockchain'));
+app.use('/api/get', require('./routes/getting'));
+app.use('/api/check', require('./routes/checking'));
 console.log("Starting the Verifish web api");
 
 var server = app.listen(8080, function() {
