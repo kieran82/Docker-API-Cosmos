3e5666eb58ec2ffa9329e80287c8f8a6c8b5a55e
kieran82 <kieran.moroney1982@gmail.com>
2020-03-20 22:11:08 +0000

    1.13

--- controller/blockchain.js
+++ controller/blockchain.js
@@ -169,7 +169,11 @@ const getitemroute = async (req, res) => {
             console.log("get " + classData.lower_name + " id " + DataIn.id + " details");
             if (!tools.isEmpty(result)) {
                 console.log("" + classData.lower_name + " " + DataIn.id + ", data: " + result + "");
-                res.send(tools.responseFormat(JSON.stringify(result), DataIn.id + " " + classData.displayName + " found", true, 200));
+                const js = JSON.parse(result);
+                let sb = new StringBuilder();
+                js.data.toString().split(",").forEach(s => sb.append(String.fromCharCode(parseInt(s, 10))));
+                let chnged = sb.toString();
+                res.send(tools.responseFormat(chnged, DataIn.id + " " + classData.displayName + " found", true, 200));
             } else {
                 console.log("" + classData.lower_name + " id " + DataIn.id + " does not exists");
                 res.status(404).send(tools.responseFormat(null, DataIn.id + " " + classData.displayName + " Not found", false, 404));
